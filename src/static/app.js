3e4ec86a57936950fe86bd42315b3c8774e5e03b
Gabally <gaballyno@gmail.com>
2022-01-12 15:40:23 +0100

    renders list of entries

--- src/static/app.js
+++ src/static/app.js
@@ -2,7 +2,8 @@ PetiteVue.createApp({
     authenticated: true,
     error: "",
     rawDB: null,
-    db: { general: { lol: 'xd', poppo: 'pop' }, lol: { cazo: 43 } },
+    db: { lol: 'xd', poppo: 'pop', lol: 'dsfdsfsdf', dsfsdf: 'dsfdsfsdf' },
+    currentEntry: null,
     getFromTextField(name) {
         let val = window.document.querySelector(`input[name="${name}"]`).value;
         window.document.querySelector(`input[name="${name}"]`).value = "";
@@ -30,12 +31,66 @@ PetiteVue.createApp({
             this.error = error;
         }
     },
+    viewEntry(name) {
+        this.currentEntry = this.db[name];
+    },
     decryptDB(e) {
         e.preventDefault();
         try {
             this.db = JSON.parse(CryptoJS.AES.decrypt(this.rawDB, this.getFromTextField("decryptpw")).toString(CryptoJS.enc.Utf8));
-        } catch(e) {
+        } catch (e) {
             this.error = "Could not decrypt the database";
         }
     }
 }).mount("#app");
+
+function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz, randomPoz + 1);
+    }
+    return randomString;
+}
+
+
+class ProtectedValue {
+    constructor(value) {
+        this.key = CryptoJS.lib.WordArray.random(256).toString();
+        this.enc = CryptoJS.AES.encrypt(value, this.key).toString();
+    }
+
+    get() {
+        return CryptoJS.AES.decrypt(this.enc, this.key).toString(CryptoJS.enc.Utf8);
+    }
+}
+
+class Entry {
+    constructor(data) {
+        if (data) {
+
+        } else {
+            this.name = `entry-${Date.now()}`;
+            this.created = Date.now();
+            this.fields = {
+                username: {
+                    type: "text",
+                    value: "mycoolusername"
+                },
+                password: {
+                    type: "protected",
+                    value: new ProtectedValue("password")
+                }
+            };
+        }
+    }
+
+    addEntry(newEntry) {
+        if (!this.fields[newEntry.name]) {
+            this.fields[newEntry.name] = newEntry.entry;
+        } else {
+            throw "Entry already exists";
+        }
+    }
+}
