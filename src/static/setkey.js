c17835f3e634616572760f5db2ba2cc7e86ce9ef
Gabally <gaballyno@gmail.com>
2022-01-14 20:54:05 +0100

    updated setkey page to vuejs

--- src/static/setkey.js
+++ src/static/setkey.js
@@ -1,25 +1,30 @@
-PetiteVue.createApp({
-    error: "",
-    getFromTextField(name) {
-        let val = window.document.querySelector(`input[name="${name}"]`).value;
-        window.document.querySelector(`input[name="${name}"]`).value = "";
-        return val;
+new Vue({
+    el: '#app',
+    data: {
+        error: ""
     },
-    async setKey(e) {
-        e.preventDefault();
-        let encDB = CryptoJS.AES.encrypt("{}", this.getFromTextField("dbkey"));
-        let resp = await (await fetch("/init", {
-            method: "POST",
-            body: JSON.stringify({ key: btoa(this.getFromTextField("key")), db: encDB.toString() }),
-            headers: {
-                "Content-Type": "application/json"
+    methods: {
+        getFromTextField(name) {
+            let val = window.document.querySelector(`input[name="${name}"]`).value;
+            window.document.querySelector(`input[name="${name}"]`).value = "";
+            return val;
+        },
+        async setKey(e) {
+            e.preventDefault();
+            let encDB = CryptoJS.AES.encrypt("{}", this.getFromTextField("dbkey"));
+            let resp = await (await fetch("/init", {
+                method: "POST",
+                body: JSON.stringify({ key: btoa(this.getFromTextField("key")), db: encDB.toString() }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })).json();
+            let { success, message } = resp;
+            if (success) {
+                window.location.reload();
+            } else {
+                this.error = message;
             }
-        })).json();
-        let { success, message } = resp;
-        if (success) {
-            window.location.reload();
-        } else {
-            this.error = message;
         }
     }
-}).mount("#app");
+});
